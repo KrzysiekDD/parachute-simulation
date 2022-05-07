348887bfb1a71d90f46773b2c01e74def5e42ccd
Krzysztof Dymanowski <krzysiekd.navy@gmail.com>
2022-05-07 15:59:33 +0100

    minor changes

--- TaylorMethod.py
+++ TaylorMethod.py
@@ -1,5 +1,6 @@
 import math
 import pygame
+
 """
 TODO this class will implement the differential equation used to determine the trajectory of the paratrooper
 and it's derivatives
@@ -8,10 +9,12 @@ Freefall with air resistance:
 Initial Conditions y(0) = y0, v(0) = y'(0) = 0
     m - mass, b - Stoke's constant, y0 - initial height, yFinal - final height,  g = 9,81 m/s^2
 """
+
+
 class TaylorMethod:
     G = 9.81
     Y_FINAL = 0
-    H = 0.05
+    H = 0.001
     N = 1000
 
     def __init__(self, m: float, b: float, y0: float):
@@ -21,20 +24,30 @@ class TaylorMethod:
         self.y_second_derivative = []
         self.y_first_derivative = []
         self.y_position = []
-        #self.y_second_derivative.append(0)
         self.y_first_derivative.append(0)
         self.y_position.append(self.y0)
 
     def numerical_method(self):
-        for i in range(TaylorMethod.N):
+        i = 0
+        while self.y_position[i] >= 0:
+
             self.y_second_derivative.append(-TaylorMethod.G
-            + (self.b/self.m) * self.y_first_derivative[i] * self.y_first_derivative[i])
+            + (self.b / self.m) * self.y_first_derivative[i] * self.y_first_derivative[i])
 
-            self.y_first_derivative.append(self.y_first_derivative[i] + TaylorMethod.H*self.y_second_derivative[i])
+            self.y_first_derivative.append(self.y_first_derivative[i] + TaylorMethod.H * self.y_second_derivative[i])
 
-            self.y_position.append(self.y_position[i] + TaylorMethod.H*self.y_first_derivative[i]
-            + TaylorMethod.H * TaylorMethod.H * 0.5*self.y_second_derivative[i])
+            x = self.y_position[i] + TaylorMethod.H * self.y_first_derivative[i]
+            + TaylorMethod.H * TaylorMethod.H * 0.5 * self.y_second_derivative[i]
+            if x <= 0:
+                i += 1
+                break
+            else:
+                self.y_position.append(x)
+            i += 1
 
-        for i in range(TaylorMethod.N):
-            print(i, "  ", self.y_position[i], "     ", self.y_first_derivative[i], "     ", self.y_second_derivative[i],
-                  sep="")
+        self.y_position.append(0)
+        self.y_second_derivative.append(self.y_second_derivative[i-1])
+
+        for j in range(len(self.y_position)):
+            print(j, "    ", self.y_position[j], "    ", self.y_first_derivative[j], "    ",
+                  self.y_second_derivative[j])
