7f42ea9a33a0152deba95e9564eff9c79d16c232
damik422 <damik422@gmail.com>
2022-06-13 21:59:37 +0200

    add simulation step size setting

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ from matplotlib.backends.backend_tkagg import (
 
 
 class Paratrooper:
-    def __init__(self, mass: float, B: float, y0: float, ax1, ax2, ax3):
+    def __init__(self, mass: float, B: float, y0: float, ax1, ax2, ax3, H):
         self.mass = mass
         self.B = B
         self.y0 = y0
-        #
-        #self.H = H
-        #
+        self.H = H
         self.ax1 = ax1
         self.ax2 = ax2
         self.ax3 = ax3
@@ -25,7 +23,7 @@ class Paratrooper:
         self.lastAcceleration = 0.0
 
     def display(self):
-        method = TaylorMethod(self.mass, self.B, self.y0) ####self.H)
+        method = TaylorMethod(self.mass, self.B, self.y0, self.H)
         method.numerical_method()
         length = list(range(len(method.y_position)))
 
@@ -46,20 +44,24 @@ class Paratrooper:
         df2 = DataFrame(self.data, columns=['Time', 'Velocity'])
         df3 = DataFrame(self.data, columns=['Time', 'Acceleration'])
 
+        df1['Time'] = self.H*df1['Time']
+        df2['Time'] = self.H * df2['Time']
+        df3['Time'] = self.H * df3['Time']
+
         # position plot
 
         df1 = df1[['Time', 'Distance']].groupby('Time').sum()
-        df1.plot(kind='line', legend=True, ax=self.ax1, color='r', fontsize=10)
+        df1.plot(kind='line', legend=True, ax=self.ax1, color='r', fontsize=10, xlabel="Time [s]", ylabel="Distance [m]")
 
         # velocity plot
 
         df2 = df2[['Time', 'Velocity']].groupby('Time').sum()
-        df2.plot(kind='line', legend=True, ax=self.ax2, color='g', fontsize=10)
+        df2.plot(kind='line', legend=True, ax=self.ax2, color='g', fontsize=10, xlabel="Time [s]", ylabel="Velocity [m/s]")
 
         # acceleration plot
 
         df3 = df3[['Time', 'Acceleration']].groupby('Time').sum()
-        df3.plot(kind='line', legend=True, ax=self.ax3, color='b', fontsize=10)
+        df3.plot(kind='line', legend=True, ax=self.ax3, color='b', fontsize=10, xlabel="Time [s]", ylabel="Acceleration [m/s^2]")
 
     def saveToCsv(self):
         df_save = DataFrame(self.data, columns=['Time', 'Velocity', 'Acceleration'])
@@ -104,7 +106,8 @@ if __name__ == "__main__":
     T4.grid(column=1, row=3)
     T4.insert(tk.END, "0.01")
 
-    figure1 = plt.Figure(figsize=(10, 3), dpi=100)
+    figure1 = plt.Figure(figsize=(10, 4), dpi=100)
+    figure1.tight_layout()
 
     line1 = FigureCanvasTkAgg(figure1, root)
 
@@ -130,7 +133,7 @@ if __name__ == "__main__":
 
     # plots
 
-    line1.get_tk_widget().grid(column=2, row=0, rowspan=5, padx=4, pady=20)
+    line1.get_tk_widget().grid(column=2, row=0, rowspan=5, padx=20, pady=20)
 
     ax1 = figure1.add_subplot(131)
     ax2 = figure1.add_subplot(132)
@@ -140,7 +143,9 @@ if __name__ == "__main__":
     ax2.set_title('Velocity vs Time')
     ax3.set_title('Acceleration vs Time')
 
-    paratrooper = Paratrooper(float(T1.get()), float(T2.get()), float(T3.get()), ax1, ax2, ax3)
+    figure1.tight_layout()
+
+    paratrooper = Paratrooper(float(T1.get()), float(T2.get()), float(T3.get()), ax1, ax2, ax3, float(T4.get()))
 
 
     def onSimulate():
@@ -160,12 +165,14 @@ if __name__ == "__main__":
         paratrooper.ax1 = ax1
         paratrooper.ax2 = ax2
         paratrooper.ax3 = ax3
+        paratrooper.H = float(T4.get())
 
         paratrooper.display()
 
         lastVel.set(repr(paratrooper.lastVelocity))
         lastAccel.set(repr(paratrooper.lastAcceleration))
 
+        figure1.tight_layout()
         line1.draw_idle()
 
 
